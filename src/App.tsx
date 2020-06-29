821122ccde3c8580d9507e4172d8ecd5898d2472
writepro4 <writepro4@naver.com>
2020-06-29 16:23:31 +0900

    state 값 알려주기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React, { Component } from "react";
-import { createGlobalStyle } from "styled-components";
+import React, {Component} from "react";
+import {createGlobalStyle} from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+//App에 state값을 알려주기 위한 용도.
+interface IState {
+    counter: number;
+}
+
+//IState 상속 받아서 state값을 알아냄
+class App extends Component<{}, IState> {
+    //stat선언
+    state = {
+        counter: 0
+    }
+
+    //render가 어플리케이션 실행할때 실행되는 것
+    render() {
+        //this로 state 값 받아서 사용
+        const {counter} = this.state;
+        //render가 실행되고 return값으로 html 객체를 리턴해줌.
+        return <div>{counter}
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
         </div>
-    );
-  }
+    }
+
+    add = () => {
+        //prev는 값을 넘겨주기 위한 변수명
+        this.setState(prev => {
+            return {
+                counter: prev.counter + 1
+            }
+        })
+    }
+
+    minus = () => {
+        //prev는 값을 넘겨주기 위한 변수명
+        this.setState(prev => {
+            return {
+                counter: prev.counter - 1
+            }
+        })
+    }
 }
 
 export default App;
